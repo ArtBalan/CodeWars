69a62738192830688b2cf84947c29513168bee3c
Arthur Balandreau <artbalan20@gmail.com>
2022-09-27 12:30:32 +0200

    a

--- multiply.js
+++ multiply.js
@@ -1,17 +1,27 @@
-function add1(a){ return a + 1 };
-function sub1(a){ return a - 1 }
+let add1 = a => a + 1;
+let sub1 = a => a - 1;
+let giveMeZero = () => 0;
 
 function addN(a,n){
   temp = a;
-  for(let i=0; i<n; i++) temp = add1(temp)
+  for(let i = giveMeZero(); i<n; i = add1(i)) temp = add1(temp)
   return temp;
 }
 
+let toStr = a => a + '';
+
 function multiply(a, b){
-  let temp = 0;
-  while (b!=0){ 
+  // Juste for optimisation sake
+  if(a == giveMeZero()) return 0;
+  if(b == giveMeZero()) return 0;
+  if(toStr(a)[0] == '-') return "can't process negative number";
+  if(toStr(b)[0] == '-') return "can't process negative numbre";
+  let temp = giveMeZero();
+  while (b!=giveMeZero()){ 
     temp = addN(temp,a);
     b = sub1(b);
   }
   return temp;
 }
+
+console.log(multiply(2,5));
